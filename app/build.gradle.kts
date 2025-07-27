81c3099cfcadd809db37a15fbbffb56d33becaad
MKTechnified <mktechnifed@gmail.com>
2025-07-27 14:11:37 +0530

    Added SplashScreen

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "itsme.mysterious.system"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "itsme.mysterious.system"
+        minSdk = 33
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    /* ssp sdp dependency */
+    implementation(libs.ssp.android)
+    implementation(libs.sdp.android)
+
+    /*SplashScreen */
+    implementation(libs.core.splashscreen)
+
+}
---
+++ app/src/main/java/itsme/mysterious/system/MainActivity.java
@@ -0,0 +1,42 @@
+package itsme.mysterious.system;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.activity.ComponentActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends ComponentActivity {
+
+    private boolean isSplashDone = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Handle SplashScreen
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        // Keep splash screen visible until condition is false
+        splashScreen.setKeepOnScreenCondition(() -> !isSplashDone);
+
+        //Set Layout Settings
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Delay for 10 seconds before proceeding
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            isSplashDone = true;
+        }, 10000); // 10000 ms = 10 sec
+    }
+}
